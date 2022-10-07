6abb7982cda33a74e806318eb9609ae01ae53ef4
Replit user <>
2022-10-07 19:17:00 +0000

    curso_acelerado_python

---
+++ sesion1/ejercicio1.py
@@ -0,0 +1,11 @@
+nombre_estado = 'Tabasco'
+superficie = 25267
+poblacion_total = 2403000000
+promedio_temperatura = 31.3
+estados_cercanos = ['chiapas','campeche','veracruz']
+produce = ['cacao','coco','caña']
+
+print(nombre_estado, " es un estado que ", )
+print("con ",estados_cercanos[0], "colinda al sur", "y")
+print(produce, "cacao")
+print("Tiene una superficie de ", superficie)
